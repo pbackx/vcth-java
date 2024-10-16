d87d78a95f77ba9ccb7dc265412c1691a7b68a0c
Peter Backx <peter.backx@gmail.com>
2024-10-16 12:10:43 +0200

    Minimal chat UI is working. Ready to be deployed so I can get a first version out.

--- src/main/java/com/peated/valhack/Main.java
+++ src/main/java/com/peated/valhack/Main.java
@@ -7,7 +7,7 @@ import com.peated.valhack.service.EsportsFixtureService;
 import com.peated.valhack.service.GameDataProvider;
 import com.peated.valhack.service.GameService;
 import com.peated.valhack.val.ValParser;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.peated.valhack.service.BedrockAgentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.io.Resource;
@@ -25,17 +25,19 @@ import java.util.List;
 @EnableAsync
 public class Main {
 
-    @Autowired
-    private GameDataProvider gameDataProvider;
+    private final GameDataProvider gameDataProvider;
+    private final ValParser valParser;
+    private final GameService gameService;
+    private final EsportsFixtureService esportsFixtureService;
+    private final BedrockAgentService bedrockAgentService;
 
-    @Autowired
-    private ValParser valParser;
-
-    @Autowired
-    private GameService gameService;
-
-    @Autowired
-    private EsportsFixtureService esportsFixtureService;
+    public Main(GameDataProvider gameDataProvider, ValParser valParser, GameService gameService, EsportsFixtureService esportsFixtureService, BedrockAgentService bedrockAgentService) {
+        this.gameDataProvider = gameDataProvider;
+        this.valParser = valParser;
+        this.gameService = gameService;
+        this.esportsFixtureService = esportsFixtureService;
+        this.bedrockAgentService = bedrockAgentService;
+    }
 
     @GetMapping("/data/{id}")
     @ResponseBody
@@ -45,8 +47,35 @@ public class Main {
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("tournaments", Arrays.stream(Tournament.values()).map(Tournament::name).toArray());
         return "index";
+    }
+
+    record UserMessageRequest(String userMessage) { }
+
+    @PostMapping("/message")
+    public String answer(@RequestBody UserMessageRequest body, Model model) {
+        model.addAttribute("message", body.userMessage());
+        model.addAttribute("response", bedrockAgentService.getResponse(body.userMessage(), "TODO"));
+
+//        var r = """
+//The team I would assemble consists of:
+//
+//Sentinel: FNC Alfajer
+//Initiator 1: DRX stax
+//Initiator 2: NRG crashies
+//Duelist: LEV aspas
+//Controller: DRX MaKo
+//
+//This composition balances the key roles needed in competitive matches. The two elite Initiators stax and crashies can effectively gather information and set up plays for the aggressive Duelist aspas to get opening picks. Alfajer as the Sentinel can anchor sites with his utility, while MaKo controls areas with his Controller smokes and abilities.\s
+//""";
+//        model.addAttribute("response", r);
+        return "message :: main";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model) {
+        model.addAttribute("tournaments", Arrays.stream(Tournament.values()).map(Tournament::name).toArray());
+        return "admin";
     }
 
     @PostMapping("/process")
---
+++ src/main/java/com/peated/valhack/service/BedrockAgentService.java
@@ -0,0 +1,59 @@
+package com.peated.valhack.service;
+
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
+import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.bedrockagentruntime.BedrockAgentRuntimeAsyncClient;
+import software.amazon.awssdk.services.bedrockagentruntime.model.InvokeAgentRequest;
+import software.amazon.awssdk.services.bedrockagentruntime.model.InvokeAgentResponseHandler;
+
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicReference;
+
+@Service
+public class BedrockAgentService {
+    private final BedrockAgentRuntimeAsyncClient bedrockAgentRuntimeClient;
+
+    public BedrockAgentService() {
+        var config = ClientOverrideConfiguration.builder()
+                .retryStrategy(r -> r.maxAttempts(1))
+                .build();
+        var httpClient = NettyNioAsyncHttpClient.builder()
+                .readTimeout(Duration.ofSeconds(900))
+                .writeTimeout(Duration.ofSeconds(900))
+                .connectionAcquisitionTimeout(Duration.ofSeconds(900))
+                .build();
+        this.bedrockAgentRuntimeClient = BedrockAgentRuntimeAsyncClient.builder()
+                .overrideConfiguration(config)
+                .httpClient(httpClient)
+                .region(Region.US_EAST_1)
+                .build();
+    }
+
+    public String getResponse(String message, String session) {
+        var finalCompletion = new AtomicReference<>("");
+
+        var request = InvokeAgentRequest.builder()
+                .agentId("XE4ZTBU4MR")
+                .agentAliasId("WZP2NIVGSH") // can also use TSTALIASID for testing https://docs.aws.amazon.com/bedrock/latest/userguide/agents-test.html
+                .inputText(message)
+                .sessionId(session)
+                .build();
+
+        var visitor = InvokeAgentResponseHandler.Visitor.builder()
+                .onChunk(chunk -> finalCompletion.set(finalCompletion.get() + chunk.bytes().asUtf8String()))
+                .build();
+
+        var handler = InvokeAgentResponseHandler.builder()
+                .subscriber(visitor)
+                .build();
+
+        bedrockAgentRuntimeClient.invokeAgent(
+                request,
+                handler
+        ).join();
+
+        return finalCompletion.get();
+    }
+}
