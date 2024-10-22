da92a39133c0b17615ac61ecda0ab7320510cfde
Peter Backx <peter.backx@gmail.com>
2024-10-22 13:58:07 +0200

    Add background + update agent to latest version that now also has access to player details

--- src/main/java/com/peated/valhack/service/BedrockAgentService.java
+++ src/main/java/com/peated/valhack/service/BedrockAgentService.java
@@ -37,7 +37,7 @@ public class BedrockAgentService {
 
         Consumer<InvokeAgentRequest.Builder> request = (InvokeAgentRequest.Builder builder) -> builder
                 .agentId("XE4ZTBU4MR")
-                .agentAliasId("5INJP6N3EH") // can also use TSTALIASID for testing https://docs.aws.amazon.com/bedrock/latest/userguide/agents-test.html
+                .agentAliasId("U1TLMI3ZDT") // can also use TSTALIASID for testing https://docs.aws.amazon.com/bedrock/latest/userguide/agents-test.html
                 .inputText(message)
                 .sessionId(session);
 
